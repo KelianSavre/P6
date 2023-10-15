0353f8a87a396c51b8e36b48b73264666faf3d15
Kélian SAVRE <SAVRE.Kelian.1S2@orange.fr>
2023-10-15 05:44:21 +0200

    Modale Suppression Fonctionnelle + Ajout changement de modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,10 +2,10 @@ const reponseworks = await fetch("http://localhost:5678/api/works")
 const works = await reponseworks.json()
 
 console.log(works)
-console.log(window.sessionStorage.getItem("token"))
+console.log(window.sessionStorage.getItem("tokenSophieBluel01"))
 
 let userIsLoggedIn
-
+/*Definit si l'utilisateur est connecté ou non en regardant si la valeur du token est differente de null */
 if(window.sessionStorage.getItem("tokenSophieBluel01")!= null){
     userIsLoggedIn=true
 }else{
@@ -13,7 +13,7 @@ if(window.sessionStorage.getItem("tokenSophieBluel01")!= null){
 }
 
 console.log(userIsLoggedIn)
-/*Declaration des elements pour les boutons filtres */
+/*Declaration des elements pour les boutons filtres et ajout des classes */
 const filtresPhoto = document.querySelector(".filtres")
 
 const boutonFiltresTous = document.createElement("button")
@@ -57,15 +57,28 @@ affichageworks(works)
 /*Fonctions de gestion de la modale*/
 let modal = null
 
+const modaleSuppressionProjet= document.getElementById("modale_Suppression")
+const modaleAjoutProjet=document.getElementById("modale_Ajout")
+
+const boutonsFermetureModale=document.querySelectorAll(".modale_Fermeture")
+const stoppeursPropagationModale=document.querySelectorAll(".modale_Stop")
+
 const openModal = function(e){
     e.preventDefault()
-    modal = document.querySelector(".modale_Suppression")
+    modal = document.querySelector(".modale")
     modal.style.display = "flex"
     modal.removeAttribute("aria-hidden")
     modal.setAttribute("aria-modal", "true")
     modal.addEventListener("click", closeModal)
-    document.querySelector(".modale_Fermeture").addEventListener("click", closeModal)
-    document.querySelector(".modale_Stop").addEventListener("click", stopPropagation)
+    
+    
+    for (let n = 0; n < boutonsFermetureModale.length; n++){
+        boutonsFermetureModale[n].addEventListener("click", closeModal)
+    }
+    for(let x = 0; x < stoppeursPropagationModale.length; x++){
+        stoppeursPropagationModale[x].addEventListener("click", stopPropagation)
+    }
+
 }
 
 const closeModal = function(e){
@@ -77,26 +90,68 @@ const closeModal = function(e){
         modal.setAttribute("aria-hidden", "true")
         modal.removeAttribute("aria-modal")
         modal.removeEventListener("click", closeModal)
-        document.querySelector(".modale_Fermeture").removeEventListener("click", closeModal)
-        document.querySelector(".modale_Stop").removeEventListener("click", stopPropagation)
+        for (let n = 0; n < boutonsFermetureModale.length; n++){
+            boutonsFermetureModale[n].removeEventListener("click", closeModal)
+        }
+        for(let x = 0; x < stoppeursPropagationModale.length; x++){
+            stoppeursPropagationModale[x].removeEventListener("click", stopPropagation)
+        }
+
         modal=null
+        modaleSuppressionProjet.style.display="flex"
+        modaleAjoutProjet.style.display="none"
     }
 }
 
 const stopPropagation = function(e){
     e.stopPropagation()
 }
-
+/*Affichage des projets dans la modale et du bouton suppression et sa fonctionnalité*/
 const affichageEtSuppressionworks = function(works){
     for (let i = 0; i < works.length; i++){
+        
+        const figureModale = document.createElement("figure")
         const GallerieModale = document.querySelector(".modale_Gallerie")
-        const worksPhoto = document.createElement("img")
-        worksPhoto.src = works[i].imageUrl
-        worksPhoto.alt = works[i].title
-        
-        GallerieModale.appendChild(worksPhoto)
-        
+        const worksModale = document.createElement("img")
+        const DivSuppression= document.createElement("div")
+        const IconeCorbeille = document.createElement("i")
+        IconeCorbeille.classList.add("fa-solid", "fa-trash-can")
+        DivSuppression.classList.add("bouton_suppression-modale")
+        worksModale.src = works[i].imageUrl
+        worksModale.alt = works[i].title
+       
+        figureModale.appendChild(worksModale)
+        DivSuppression.appendChild(IconeCorbeille)
+        figureModale.appendChild(DivSuppression)
 
+        GallerieModale.appendChild(figureModale)
+        
+        DivSuppression.addEventListener("click",async function(event){
+            
+            event.preventDefault()
+            event.stopPropagation()
+            const IDSuppression = works[i].id
+            console.log(IDSuppression)
+            let Token= window.sessionStorage.getItem("tokenSophieBluel01")
+
+            await fetch(`http://localhost:5678/api/works/${IDSuppression}`,{
+                method:"DELETE",
+                headers:{
+                    accept:"*/*",
+                    Authorization: `Bearer ${Token}`,
+                    
+                }
+            }).then (function(response){
+                if(response.status>=200 && response.status<300){
+                    return false
+                    
+                    
+                }else{
+                    alert("Echec de la suppression du projet")
+                }
+            })
+
+        })
         
 
     }
@@ -132,15 +187,39 @@ if (userIsLoggedIn === true){
 
     LienLoginLogout.href=""
     LienLoginLogout.innerText="logout"
-    
+    /*Suppression du token au click sur le logout et rechargement de la page */
     LienLoginLogout.addEventListener("click", function(){
         window.sessionStorage.removeItem("tokenSophieBluel01")
         document.location.href="index.html"
     })
-    /*Event Listener pour ouverture modale*/
+    /*Event Listener pour ouverture modale sur la div*/
     divModificationProjet.addEventListener("click", openModal)
 
+    /*Appel de la fonction d'affichage des element et de suppression des projets dans la modale */
     affichageEtSuppressionworks(works)
+    /*Ajout du bouton pour acceder a la seconde modale et fonctionnement */
+    const boutonChangementModale = document.createElement("button")
+    boutonChangementModale.classList.add("bouton_actif")
+    boutonChangementModale.innerText="Ajouter une photo"
+    
+    
+    modaleSuppressionProjet.appendChild(boutonChangementModale)
+
+    boutonChangementModale.addEventListener("click", function(event){
+        event.preventDefault()
+        event.stopPropagation()
+        modaleSuppressionProjet.style.display="none"
+        modaleAjoutProjet.style.display="flex"
+    })
+
+    const boutonRetourModalePrecedente=document.querySelector(".modale_Retour")
+    boutonRetourModalePrecedente.addEventListener("click",function(event){
+        event.preventDefault()
+        event.stopPropagation()
+        modaleSuppressionProjet.style.display="flex"
+        modaleAjoutProjet.style.display="none"
+    })
+
 }else{
     filtresPhoto.appendChild(boutonFiltresTous)
     filtresPhoto.appendChild(boutonFiltresObjets)
